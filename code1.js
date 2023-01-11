cf8efa3b946e7587b58445335591a77209b9f24c
AadrikaRanjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2023-01-11 23:44:43 +0530

    Update code1.js

--- code1.js
+++ code1.js
@@ -1,15 +1,15 @@
 //printing pattern
 
-let n = 5;
-let string = "";
+var n = 5;
+var string = "";
 // External loop
-for (let i = 1; i <= n; i++) {
+for (var i = 1; i <= n; i++) {
   // printing spaces
-  for (let j = 1; j <= n - i; j++) {
+  for (var j = 1; j <= n - i; j++) {
     string += " ";
   }
   // printing star
-  for (let k = 0; k < 2 * i - 1; k++) {
+  for (var k = 0; k < 2 * i - 1; k++) {
     string += "*";
   }
   string += "\n";
